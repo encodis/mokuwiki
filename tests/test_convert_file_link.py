4d89f1692a5eeb544bcb9ea3d705e433c749f47a
Philip Hodder <philip.hodder@encodis.com>
2019-04-16 23:16:41 +0100

    Completed file link tests

--- tests/test_convert_file_link.py
+++ tests/test_convert_file_link.py
@@ -4,20 +4,16 @@ from mokuwiki import mokuwiki
 
 
 def test_convert_file_link(tmpdir):
-
-    ### DOES NOT WORK because mokuwiki() reads from os.getcwd() with is project folder not tmpdir
-    ### add option for specifying root of include files? (-i, --include)?
-
     source_dir = tmpdir.mkdir('source')
 
     file1 = source_dir.join('file1.md')
-    file1.write('''---
+    file1.write(f'''---
 title: Page One
 author: Phil
 tags: [abc]
 ...
 
-<<./file2.md>>
+<<{tmpdir}/source/file2.md>>
 ''')
 
     file2 = source_dir.join('file2.md')
@@ -28,6 +24,7 @@ tags: [abc]
 ...
 
 Contents of Page Two
+
 ''')
 
     target_dir = tmpdir.mkdir('target')
@@ -44,33 +41,265 @@ author: Phil
 tags: [abc]
 ...
 
+
+
 Contents of Page Two
+
+
+
 
 '''
 
     with open(os.path.join(target_dir, 'page_one.md'), 'r', encoding='utf8') as fh:
         actual = fh.read()
 
-    #assert expect == actual
-    assert True
+    assert expect == actual
 
 
 def test_convert_file_link_globbing(tmpdir):
-    assert True
+    source_dir = tmpdir.mkdir('source')
+
+    file1 = source_dir.join('file1.md')
+    file1.write(f'''---
+title: Page One
+author: Phil
+tags: [abc]
+...
+
+<<{tmpdir}/source/fileX*.md>>
+''')
+
+    file2 = source_dir.join('fileX2.md')
+    file2.write('''---
+title: Page Two
+author: Phil
+tags: [abc]
+...
+
+Contents of Page Two
+
+''')
+
+    file3 = source_dir.join('fileX3.md')
+    file3.write('''---
+title: Page Three
+author: Phil
+tags: [abc]
+...
+
+Contents of Page Three
+
+''')
+
+    target_dir = tmpdir.mkdir('target')
+
+    mokuwiki(source_dir, target_dir)
+
+    # assert correct output files exist
+    assert os.path.exists(os.path.join(target_dir, 'page_one.md'))
+
+    # assert contents of page_one.md have a link to page_two.md
+    expect = '''---
+title: Page One
+author: Phil
+tags: [abc]
+...
 
 
-def test_convert_file_link_hidden(tmpdir):
-    # files starting with . are not included?
-    assert True
+
+Contents of Page Two
+
+
+
+
+
+
+
+Contents of Page Three
+
+
+
+
+'''
+
+    with open(os.path.join(target_dir, 'page_one.md'), 'r', encoding='utf8') as fh:
+        actual = fh.read()
+
+    assert expect == actual
 
 
 def test_convert_file_link_separator(tmpdir):
-    assert True
+    source_dir = tmpdir.mkdir('source')
+
+    file1 = source_dir.join('file1.md')
+    file1.write(f'''---
+title: Page One
+author: Phil
+tags: [abc]
+...
+
+<<{tmpdir}/source/fileX*.md|* * *>>
+''')
+
+    file2 = source_dir.join('fileX2.md')
+    file2.write('''---
+title: Page Two
+author: Phil
+tags: [abc]
+...
+Contents of Page Two
+''')
+
+    file3 = source_dir.join('fileX3.md')
+    file3.write('''---
+title: Page Three
+author: Phil
+tags: [abc]
+...
+Contents of Page Three
+''')
+
+    target_dir = tmpdir.mkdir('target')
+
+    mokuwiki(source_dir, target_dir)
+
+    # assert correct output files exist
+    assert os.path.exists(os.path.join(target_dir, 'page_one.md'))
+
+    # assert contents of page_one.md have a link to page_two.md
+    expect = '''---
+title: Page One
+author: Phil
+tags: [abc]
+...
+
+
+Contents of Page Two
+
+
+* * *
+
+
+Contents of Page Three
+
+
+
+'''
+
+    with open(os.path.join(target_dir, 'page_one.md'), 'r', encoding='utf8') as fh:
+        actual = fh.read()
+
+    assert expect == actual
 
 
 def test_convert_file_link_prefix(tmpdir):
-    assert True
+    source_dir = tmpdir.mkdir('source')
+
+    file1 = source_dir.join('file1.md')
+    file1.write(f'''---
+title: Page One
+author: Phil
+tags: [abc]
+...
+
+<<{tmpdir}/source/file2.md||> >>
+''')
+
+    file2 = source_dir.join('file2.md')
+    file2.write('''---
+title: Page Two
+author: Phil
+tags: [abc]
+...
+Contents of Page Two
+''')
+
+    target_dir = tmpdir.mkdir('target')
+
+    mokuwiki(source_dir, target_dir)
+
+    # assert correct output files exist
+    assert os.path.exists(os.path.join(target_dir, 'page_one.md'))
+
+    # assert contents of page_one.md has contents of file2.md
+    expect = '''---
+title: Page One
+author: Phil
+tags: [abc]
+...
+
+> 
+> Contents of Page Two
+> 
+
+
+'''
+
+    with open(os.path.join(target_dir, 'page_one.md'), 'r', encoding='utf8') as fh:
+        actual = fh.read()
+
+    assert expect == actual
 
 
 def test_convert_file_link_separator_and_prefix(tmpdir):
-    assert True
+    source_dir = tmpdir.mkdir('source')
+
+    file1 = source_dir.join('file1.md')
+    file1.write(f'''---
+title: Page One
+author: Phil
+tags: [abc]
+...
+
+<<{tmpdir}/source/fileX*.md|* * *|> >>
+''')
+
+    file2 = source_dir.join('fileX2.md')
+    file2.write('''---
+title: Page Two
+author: Phil
+tags: [abc]
+...
+Contents of Page Two
+''')
+
+    file3 = source_dir.join('fileX3.md')
+    file3.write('''---
+title: Page Three
+author: Phil
+tags: [abc]
+...
+Contents of Page Three
+''')
+
+    target_dir = tmpdir.mkdir('target')
+
+    mokuwiki(source_dir, target_dir)
+
+    # assert correct output files exist
+    assert os.path.exists(os.path.join(target_dir, 'page_one.md'))
+
+    # assert contents of page_one.md have a link to page_two.md
+    expect = '''---
+title: Page One
+author: Phil
+tags: [abc]
+...
+
+> 
+> Contents of Page Two
+> 
+
+* * *
+
+> 
+> Contents of Page Three
+> 
+
+
+'''
+
+    with open(os.path.join(target_dir, 'page_one.md'), 'r', encoding='utf8') as fh:
+        actual = fh.read()
+
+    assert expect == actual
