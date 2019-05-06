3d6dafdb55d1318a4380adcbc966f182e2accd39
Philip Hodder <philip.hodder@encodis.com>
2019-05-06 20:34:01 +0100

    Updated tests

--- tests/test_convert_tags_link.py
+++ tests/test_convert_tags_link.py
@@ -67,6 +67,66 @@ Pages containing tag 'abc':
     assert expect == actual
 
 
+def test_convert_tags_link_no_tag(tmpdir):
+    source_dir = tmpdir.mkdir('source')
+
+    file1 = source_dir.join('file1.md')
+    file1.write('''---
+title: Page One
+author: Phil
+...
+
+Pages containing tag 'abc':
+
+{{abc}}
+''')
+
+    file2 = source_dir.join('file2.md')
+    file2.write('''---
+title: Page Two
+author: Phil
+tags: [abc]
+...
+
+This is Page Two
+''')
+
+    file3 = source_dir.join('file3.md')
+    file3.write('''---
+title: Page Three
+author: Phil
+tags: [xyz]
+...
+
+This is Page Three
+''')
+
+    target_dir = tmpdir.mkdir('target')
+
+    mokuwiki(source_dir, target_dir)
+
+    # assert correct output files exist
+    assert os.path.exists(os.path.join(target_dir, 'page_one.md'))
+
+    # assert contents of page_one.md have a link to page_two.md
+    expect = '''---
+title: Page One
+author: Phil
+...
+
+Pages containing tag 'abc':
+
+[Page Two](page_two.html)
+
+
+'''
+
+    with open(os.path.join(target_dir, 'page_one.md'), 'r', encoding='utf8') as fh:
+        actual = fh.read()
+
+    assert expect == actual
+
+
 def test_convert_tags_link_or(tmpdir):
     source_dir = tmpdir.mkdir('source')
 
@@ -437,6 +497,7 @@ Pages containing all tags:
         actual = fh.read()
 
     assert expect == actual
+
 
 def test_convert_tags_link_list(tmpdir):
     source_dir = tmpdir.mkdir('source')
