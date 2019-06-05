32fe6bd13b7f1a88fbcceff983acecbd23fc3625
Philip Hodder <philip.hodder@encodis.com>
2019-06-05 18:31:33 +0100

    Refactor setup to use setuptools, console script

--- setup.py
+++ setup.py
@@ -1,8 +1,19 @@
-from distutils.core import setup
+"""Convert a folder of Markdown documents, replacing inter-page link and tag markup with Markdown links and lists
+
+"""
+
+from setuptools import setup
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(name='mokuwiki',
       version='1.0.0',
       description='Convert a folder of Markdown documents, replacing inter-page link and tag markup with Markdown links and lists',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       url='https://github.com/encodis/mokuwiki',
       author='Philip Hodder',
       author_email='philip.hodder@encodis.com',
@@ -16,4 +27,12 @@ setup(name='mokuwiki',
       ],
       keywords='markdown wiki converter',
       py_modules=['mokuwiki'],
+      install_requires=[
+        'pyyaml>=5.1'
+      ],
+      entry_points={
+        'console_scripts': [
+            'mokuwiki = mokuwiki:main',
+        ],
+      }
       )
